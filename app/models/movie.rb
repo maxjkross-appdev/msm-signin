1cbda2ad9a85644fc2cb5381e67a24e28f6f370c
maxjkross <maxjkross@uchicago.edu>
2023-02-24 00:16:55 +0000

    before bookmarks

---
+++ app/models/movie.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  duration    :integer
+#  image       :string
+#  title       :string
+#  year        :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  director_id :integer
+#
+class Movie < ApplicationRecord
+  validates(:title, {:presence => true})
+
+  has_many(:actors, { :through => :characters, :source => :actor })
+  belongs_to(:director, { :required => true, :class_name => "Director", :foreign_key => "director_id" })
+  has_many(:characters, { :class_name => "Character", :foreign_key => "movie_id", :dependent => :destroy })
+end
